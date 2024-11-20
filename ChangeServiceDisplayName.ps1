925f343d75d5657e30f7b835d11ee02cb58e5f32
FaronicsProServices <proservices@faronics.com>
2024-11-21 01:00:57 +0530

    Create ChangeServiceDisplayName.ps1

---
+++ ChangeServiceDisplayName.ps1
@@ -0,0 +1,4 @@
+# Changes the display name of the specified service. 
+# Example: Set the display name of the Print Spooler Service name to Printsp
+# Set-Service -Name "Spooler" -DisplayName "PrintSp"
+Set-Service -Name "servicename" -DisplayName "newdisplayname"
